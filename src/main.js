81bd5f82139786a1140d63f28cdad574f14d253a
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-10-09 19:01:01 +0200

    Display movie information with mustache templates

--- src/main.js
+++ src/main.js
@@ -7,19 +7,45 @@ function searchMovie(){
 
     console.log(plusTitle);
 
-    $.getJSON("http://www.omdbapi.com/?t="+title+"&y="+year+"&plot=short&r=json",
+    $.getJSON("http://www.omdbapi.com/?t="+title+"&y="+year+"&plot=full&r=json",
         function( data ) {
             console.log(data);
-            var items = [];
-            $.each( data, function( key, val ) {
-                items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });
 
-      $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-      }).appendTo( "body" );
-    });
+            var template = $('#movie').html();
+            Mustache.parse(template);   // optional, speeds up future uses
+            var rendered = Mustache.render(template,
+                {
+                    title: data.Title,
+                    year: data.Year,
+                    rated: data.Rated,
+                    released: data.Released,
+                    runtime: data.Runtime,
+                    genre: data.Genre,
+                    director: data.Director,
+                    writer: data.Writer,
+                    actors: data.Actors,
+                    plot: data.Plot,
+                    language: data.Language,
+                    country: data.Country,
+                    awards: data.Awards,
+                    poster: data.Poster,
+                    metascore: data.Metascore,
+                    imdbscore: data.imdbRating,
+                    imdbvotes: data.imdbVotes,
+                    imdbid: data.imdbID,
+                    type: data.Type,
+                    response: data.Response
+                });
+            $('#searchResult').html(rendered);
+            // var items = [];
+            // $.each( data, function( key, val ) {
+            //     items.push( "<li id='" + key + "'>" + val + "</li>" );
+            // });
+
+            // $( "<ul/>", {
+            //     "class": "my-new-list",
+            //     html: items.join( "" )
+            // }).appendTo( "body" );
+        }
+    );
 }
-
-
